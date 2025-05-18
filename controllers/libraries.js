a80a34431babbbe4cf9fcb0cf073770d75eb428a
maryam027 <mariam.ali027@outlook.com>
2025-05-18 09:53:44 +0300

    Add route to display all libraries with owner data populated

--- controllers/libraries.js
+++ controllers/libraries.js
@@ -3,3 +3,9 @@ const router = express.Router();
 const Library = require('../models/library');
 const multer = require('../config/multer'); // Multer for image upload
 const isSignedIn = require('../middleware/is-signed-in');
+
+// GET /libraries - Show all libraries
+router.get('/', async (req, res) => {
+  const libraries = await Library.find().populate('owner');
+  res.render('libraries/index.ejs', { libraries });
+});
