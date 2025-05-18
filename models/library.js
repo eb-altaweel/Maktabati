2ea8534c49e2c875c326608d9b25b40c57522e2b
maryam027 <mariam.ali027@outlook.com>
2025-05-18 09:19:28 +0300

    Define Library Schema

--- models/library.js
+++ models/library.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose');
+
+const librarySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  amenities: { type: [String] },
+  seatingAvailability: {
+    type: String,
+    enum: ['Limited', 'Moderate', 'Spacious'],
+    default: 'Moderate'
+  },
+  operatingHours: { type: String, required: true },
+  image: { type: String, default: 'default-library.jpg' },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
