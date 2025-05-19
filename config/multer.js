e767d7a9cbf211e84dd5c2b91f42dfd955097152
SAljar99 <saudaljar2002@gmail.com>
2025-05-19 11:18:42 +0300

    removerd /ijfif

--- config/multer.js
+++ config/multer.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
 
 // Check file type
 function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif|jifif/
+  const filetypes = /jpeg|jpg|png|gif/
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
   const mimetype = filetypes.test(file.mimetype)
 
